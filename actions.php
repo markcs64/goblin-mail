5baf4031f141fbf0e350bce86fc9f93b2ad1a628
snow.xn <snow.xn@cc2811a0-dee6-11dd-993c-4d20d8c3c59d>
2009-01-15 18:12:56 +0000

    缓存能通过json取得了/

--- actions.php
+++ actions.php
@@ -1,6 +1,4 @@
 <?php
-echo "121212";
-exit();
 //控制返回器
 if(!isset($_REQUEST["action"]))exit();/* action必需 结尾/必需 */
 $action = $_REQUEST["action"];
@@ -49,7 +47,7 @@ switch(strtoupper($action))
 		break;
 	case "NEWFILETREE" : /* path必需 结尾/必需 */
 		header("Content-Type: text/html; charset=$serverCharCode");
-		if(!isset($_REQUEST["path"]))exit();
+		if(!isset($_REQUEST["path"]))return;
 		
 		$jsonFolderPart = "";
 		$jsonFilesPart = "";
@@ -57,9 +55,16 @@ switch(strtoupper($action))
 		$path = $_REQUEST['path'];
 		if( file_exists($path) ) {
 			$files = scandir($path);
+			
+			//是否从缓存获取
+			$cachePath = $path."cache.d6w";
+			if(file_exists($cachePath) && (filemTime($cachePath) >= filemTime($path.".")) ){
+				echo file_get_contents($cachePath);
+				return;
+			}
+			
 			//natcasesort($files);
 			if( count($files) > 2 ) { /* 这2个是 ./ 和 ../ */
-				
 				foreach( $files as $file ) {
 					//排除 ./ & ../
 					if($file == "." || $file == "..") continue;
@@ -67,18 +72,27 @@ switch(strtoupper($action))
 					$filePath = $path.$file;
 					//目录
 					if(is_dir($filePath)){
-						$jsonFolderPart .= "'$filePath':{name:'$file'},";
+						if($file != ".svn"){
+								$jsonFolderPart .= "'$filePath':{name:'$file'},\n";
+						}
 					}else{
 						$ext = end(explode(".",$file));
-						$fileSize = number_format(fileSize($path.$file) / 1024,1,".","") . "kb";
-						$modifyTime = date("Y-m-d H:i:s",filemTime($path.$file));
-						$jsonFilesPart .= "'$filePath':{name:'$file',size:$fileSize,ext:'$ext',mTime:'$modifyTime'},";
+						if(empty($_POST['allowExt']) || strrpos($_POST['allowExt'],"|".$ext."|") > 0){
+							$fileSize = number_format(fileSize($path.$file) / 1024,1,".","") . "kb";
+							$modifyTime = date("Y-m-d H:i:s",filemTime($path.$file));
+							$jsonFilesPart .= "'$filePath':{name:'$file',size:'$fileSize',ext:'$ext',mTime:'$modifyTime'},\n";
+						}
 					}
 				}
 				
-				//** 最后输出  如果有 去掉最后一个,
-				echo "{folders:{".rtrim($jsonFolderPart,",")."},files:{".rtrim($jsonFilesPart,",")."}}";
-				
+				//** 最后输出  如果有 去掉最后一个,\n
+				$response = "folders:{\n".rtrim($jsonFolderPart,",\n")."\n},\nfiles:{".rtrim($jsonFilesPart,",\n")."\n}";
+				echo "({".$response."})";
+
+				//生成缓存
+				$dirCacheFile=fopen($cachePath, "wb");
+				fputs($dirCacheFile, "({".$response.",\nfromCache:true})"); 
+				fclose($dirCacheFile);unset($dirCacheFile);
 			}
 		}
 		unset($files);
